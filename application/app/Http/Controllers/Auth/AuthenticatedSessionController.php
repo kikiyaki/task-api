0cc594460f227872cf17c312150b385ce2b5d42c
kikiyaki <krl9628@gmail.com>
2021-03-18 01:24:38 +0300

    login

--- application/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ application/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -23,16 +26,18 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      *
-     * @param  \App\Http\Requests\Auth\LoginRequest  $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @param Request  $request
+     * @return Response
      */
-    public function store(LoginRequest $request)
+    public function store(Request $request)
     {
-        $request->authenticate();
+        if (Auth::attempt($request->only('email', 'password'), true)) {
+            $request->session()->regenerate();
 
-        $request->session()->regenerate();
+            return response(null, 200);
+        }
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return response(null, 401);
     }
 
     /**
