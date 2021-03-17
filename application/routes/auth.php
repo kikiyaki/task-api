899f90d5e8708d024f9997e39e30e879274dafd9
kikiyaki <krl9628@gmail.com>
2021-03-18 00:59:26 +0300

    getting csrf token route

--- application/routes/auth.php
+++ application/routes/auth.php
@@ -10,6 +10,10 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/token', function() {
+    return csrf_token();
+});
+
 Route::get('/register', [RegisteredUserController::class, 'create'])
                 ->middleware('guest')
                 ->name('register');
