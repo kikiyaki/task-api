b1451dfa03fc20a9e346c72d8aa6555838d37fc2
kikiyaki <krl9628@gmail.com>
2021-03-21 00:45:06 +0300

    add user_id column to tasks

---
+++ application/database/migrations/2021_03_20_212614_add_user_id_column_to_tasks.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdColumnToTasks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->bigInteger('user_id')->default(1);
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+}
